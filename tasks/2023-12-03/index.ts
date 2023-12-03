e6e007dd11f70a1da005a2a86c209b25af6d9495
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-03 12:49:18 +0100

    Add interface and function to find the best location

--- tasks/2023-12-03/index.ts
+++ tasks/2023-12-03/index.ts
@@ -1,2 +1,35 @@
-export function znajdzWorek(lokalizacje: Lokalizacja[], mapa: MapaCzasoprzestrzenna): Lokalizacja | null {
-}
+export interface Lokalizacja {
+    x: number;
+    y: number;
+    z: number;
+    czas: number;
+  }
+  
+  export type MapaCzasoprzestrzenna = (x: number, y: number, z: number, czas: number) => number;
+  
+  export function znajdzWorek(lokalizacje: Lokalizacja[], mapa: MapaCzasoprzestrzenna): Lokalizacja | null {
+    if (lokalizacje.length === 0) {
+      return null;
+    }
+  
+    let najlepszaLokalizacja: Lokalizacja | null = null;
+    let najwyzszaWartosc: number = Number.NEGATIVE_INFINITY;
+  
+    for (const lokalizacja of lokalizacje) {
+      const { x, y, z, czas } = lokalizacja;
+      const wartoscMapy = mapa(x, y, z, czas);
+  
+      if (isNaN(wartoscMapy) || !isFinite(wartoscMapy)) {
+        // Ignoruj niepoprawne wartości matematyczne
+        continue;
+      }
+  
+      if (wartoscMapy > najwyzszaWartosc) {
+        najwyzszaWartosc = wartoscMapy;
+        najlepszaLokalizacja = lokalizacja;
+      }
+    }
+  
+    return najlepszaLokalizacja;
+  }
+  
